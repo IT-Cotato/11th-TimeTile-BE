6ac150d4a2bcd1b22762ce3076c2161fb4674366
kych4n <hades1@kakao.com>
2025-06-25 21:02:04 +0900

    Init [#1] 프로젝트 기본 구조 설정

---
+++ src/main/java/cotato/timetile/TimetileApplication.java
@@ -0,0 +1,13 @@
+package cotato.timetile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimetileApplication.class, args);
+    }
+
+}
