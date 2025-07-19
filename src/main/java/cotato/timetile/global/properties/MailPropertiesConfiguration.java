0023d45e5377fa9eea100467b075706ea7d0e541
kych4n <hades1@kakao.com>
2025-07-19 18:12:43 +0900

    Chore [#4] 메일 설정 경로 수정

--- src/main/java/cotato/timetile/global/properties/MailPropertiesConfiguration.java
+++ src/main/java/cotato/timetile/global/properties/MailPropertiesConfiguration.java
@@ -4,6 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationPropertiesScan(basePackages = "cotato.timetile.auth.mail")
+@ConfigurationPropertiesScan(basePackages = "cotato.timetile.global.properties")
 public class MailPropertiesConfiguration {
 }
