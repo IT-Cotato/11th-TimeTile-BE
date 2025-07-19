e95ae24e10eceb030dc15542b307b551da4748db
kych4n <hades1@kakao.com>
2025-07-19 16:47:09 +0900

    Feat [#4] 유저 클래스 업데이트
    - 관련 연관성을 추가했습니다.
    - 역할에는 CURATOR가 제외되고, ADMIN이 추가되었습니다.
    - 빠른 조회를 위해 팔로워, 팔로잉 수를 추가했습니다.
    - 공개 범위를 추가했습니다.

--- src/main/java/cotato/timetile/domain/user/domain/User.java
+++ src/main/java/cotato/timetile/domain/user/domain/User.java
@@ -1,6 +1,15 @@
 package cotato.timetile.domain.user.domain;
 
+import cotato.timetile.domain.artist.domain.ArtistFollow;
+import cotato.timetile.domain.comment.domain.Comment;
+import cotato.timetile.domain.comment.domain.CommentLike;
+import cotato.timetile.domain.event.domain.Event;
+import cotato.timetile.domain.post.domain.Post;
+import cotato.timetile.domain.post.domain.PostLike;
+import cotato.timetile.domain.scrap.domain.ScrapFolder;
 import cotato.timetile.global.common.TimeInfo;
+import cotato.timetile.global.common.Visibility;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
@@ -9,7 +18,10 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -56,8 +68,37 @@ public class User {
     @Column(columnDefinition = "CHAR(7)", nullable = false)
     private Role role;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Visibility visibility;
+
     @Embedded
     private TimeInfo timeInfo;
+
+    private int followingCount;
+
+    private int followerCount;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<Event> events = new ArrayList<>();
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<Post> posts = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<PostLike> postLikes = new ArrayList<>();
+
+    @OneToMany(mappedBy = "commenter", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<CommentLike> commentLikes = new ArrayList<>();
+
+    @OneToMany(mappedBy = "creator", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<ScrapFolder> scrapFolders = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<ArtistFollow> artistFollows = new ArrayList<>();
 
     public void updateProfile(String nickname, String introduction, String imageKey) {
         if (StringUtils.hasText(nickname)) {
@@ -69,6 +110,78 @@ public class User {
         if (StringUtils.hasText(imageKey)) {
             this.imageKey = imageKey;
         }
+    }
+
+    public void write(Event event) {
+        this.events.add(event);
+    }
+
+    public void delete(Event event) {
+        this.events.remove(event);
+    }
+
+    public void write(Post post) {
+        this.posts.add(post);
+    }
+
+    public void delete(Post post) {
+        this.posts.remove(post);
+    }
+
+    public void like(PostLike postLike) {
+        this.postLikes.add(postLike);
+    }
+
+    public void like(CommentLike commentLike) {
+        this.commentLikes.add(commentLike);
+    }
+
+    public void cancelLike(PostLike postLike) {
+        this.postLikes.remove(postLike);
+    }
+
+    public void cancelLike(CommentLike commentLike) {
+        this.commentLikes.remove(commentLike);
+    }
+
+    public void comment(Comment comment) {
+        this.comments.add(comment);
+    }
+
+    public void removeComment(Comment comment) {
+        this.comments.remove(comment);
+    }
+
+    public void createScrapFolder(ScrapFolder scrapFolder) {
+        this.scrapFolders.add(scrapFolder);
+    }
+
+    public void removeScrapFolder(ScrapFolder scrapFolder) {
+        this.scrapFolders.remove(scrapFolder);
+    }
+
+    public void follow(ArtistFollow artistFollow) {
+        this.artistFollows.add(artistFollow);
+    }
+
+    public void unfollow(ArtistFollow artistFollow) {
+        this.artistFollows.remove(artistFollow);
+    }
+
+    public void increaseFollowingCount() {
+        this.followingCount++;
+    }
+
+    public void decreaseFollowingCount() {
+        this.followingCount--;
+    }
+
+    public void increaseFollowerCount() {
+        this.followerCount++;
+    }
+
+    public void decreaseFollowerCount() {
+        this.followerCount--;
     }
 
 }
