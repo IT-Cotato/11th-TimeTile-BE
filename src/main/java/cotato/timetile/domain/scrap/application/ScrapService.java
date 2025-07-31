81c033f6c6aba4547b2707b4932c695d40cb7d1e
kych4n <hades1@kakao.com>
2025-08-01 00:47:30 +0900

    Refactor [#8] 스크랩 로직 수정
    - 스크랩 폴더 내 초기화하고 추가하는 방식이 아닌, 존재 여부를 확인하고 추가 여부를 결정하는 방식으로 수정했습니다.

--- src/main/java/cotato/timetile/domain/scrap/application/ScrapService.java
+++ src/main/java/cotato/timetile/domain/scrap/application/ScrapService.java
@@ -7,12 +7,13 @@ import cotato.timetile.domain.scrap.api.request.ScrapRequest;
 import cotato.timetile.domain.scrap.api.response.ScrapStatusLoadResponse;
 import cotato.timetile.domain.scrap.domain.Scrap;
 import cotato.timetile.domain.scrap.domain.ScrapFolder;
-import cotato.timetile.domain.scrap.persistence.ScrapFolderRepository;
-import cotato.timetile.domain.scrap.persistence.ScrapRepository;
 import cotato.timetile.domain.user.domain.User;
 import cotato.timetile.domain.user.persistence.UserRepository;
+import cotato.timetile.global.exception.ForbiddenException;
 import cotato.timetile.global.exception.NotFoundException;
 import cotato.timetile.global.exception.UnauthorizedException;
+import java.util.HashSet;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,8 +22,6 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ScrapService {
 
-    private final ScrapRepository scrapRepository;
-    private final ScrapFolderRepository scrapFolderRepository;
     private final UserRepository userRepository;
     private final PostRepository postRepository;
 
@@ -39,18 +38,28 @@ public class ScrapService {
     @Transactional
     public void scrap(ScrapRequest request, Long postId, Long userId) {
         User scrapper = userRepository.findById(userId).orElseThrow(UnauthorizedException::failed);
-        scrapper.getScrapFolders().forEach(scrapFolder -> {
-            scrapRepository.findByPostIdAndScrapFolderId(postId, scrapFolder.getId()).ifPresent(scrap -> {
-                scrap.getPost().decreaseScrapCount();
-                scrapFolder.removeScrap(scrap);
-            });
-        });
-        request.scrapFolderIds().forEach(scrapFolderId -> {
-            ScrapFolder scrapFolder = scrapFolderRepository.findById(scrapFolderId)
-                    .orElseThrow(NotFoundException::wrong);
-            Post post = postRepository.findById(postId).orElseThrow(NotFoundException::wrong);
-            scrapFolder.addScrap(Scrap.of(scrapFolder, post));
-            post.increaseScrapCount();
+        Post post = postRepository.findById(postId).orElseThrow(NotFoundException::wrong);
+        List<ScrapFolder> scrapFolders = scrapper.getScrapFolders();
+        validateScrapFolders(scrapFolders, request.scrapFolderIds());
+        updateScraps(scrapFolders, post, request.scrapFolderIds());
+    }
+
+    private void validateScrapFolders(List<ScrapFolder> scrapFolders, List<Long> requestedFolderIds) {
+        if (!new HashSet<>(scrapFolders.stream().map(ScrapFolder::getId).toList())
+                .containsAll(requestedFolderIds)) {
+            throw ForbiddenException.wrong();
+        }
+    }
+
+    private void updateScraps(List<ScrapFolder> scrapFolders, Post post, List<Long> targetFolderIds) {
+        scrapFolders.forEach(scrapFolder -> {
+            if (targetFolderIds.contains(scrapFolder.getId()) && !scrapFolder.alreadyScrapped(post.getId())) {
+                scrapFolder.addScrap(Scrap.of(scrapFolder, post));
+                post.increaseScrapCount();
+            } else if (!targetFolderIds.contains(scrapFolder.getId()) && scrapFolder.alreadyScrapped(post.getId())) {
+                scrapFolder.getScraps().removeIf(scrap -> scrap.getPost().equals(post));
+                post.decreaseScrapCount();
+            }
         });
     }
 
