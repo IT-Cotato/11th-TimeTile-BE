bc1ef87a399d3fb6af1f7c92f6b62274d32fa54f
kych4n <hades1@kakao.com>
2025-08-07 02:52:01 +0900

    Refactor [#13] id 필드 타입 Keyword 지정
    - 연산의 용이함과 분석의 불필요함으로 Keyword로 지정했습니다.

--- src/main/java/cotato/timetile/domain/post/domain/PostDocument.java
+++ src/main/java/cotato/timetile/domain/post/domain/PostDocument.java
@@ -1,30 +1,51 @@
 package cotato.timetile.domain.post.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.InnerField;
+import org.springframework.data.elasticsearch.annotations.MultiField;
+import org.springframework.data.elasticsearch.annotations.Setting;
+
 
 @Document(indexName = "posts")
+@Setting(settingPath = "elasticsearch/post-settings.json")
+@JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@Getter
+@Builder
 public class PostDocument {
 
     @Id
+    @Field(type = FieldType.Keyword)
     private String id;
 
-    @Field(type = FieldType.Text, analyzer = "nori")
+    @MultiField(
+            mainField = @Field(type = FieldType.Text, analyzer = "korean_english_analyzer"),
+            otherFields = {
+                    @InnerField(suffix = "keyword", type = FieldType.Keyword),
+                    @InnerField(suffix = "edge", type = FieldType.Text, analyzer = "edge_ngram_analyzer")
+            }
+    )
     private String title;
 
-    @Field(type = FieldType.Text, analyzer = "nori")
+    @Field(type = FieldType.Text, analyzer = "korean_english_analyzer")
     private String content;
 
     public static PostDocument of(Post post) {
-        return new PostDocument(
-                post.getId().toString(),
-                post.getTitle(),
-                post.getContent()
-        );
+        return PostDocument.builder()
+                .id(post.getId().toString())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .build();
     }
 
 }
