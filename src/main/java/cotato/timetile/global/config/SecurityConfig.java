e051284475fa74ad65e7dff06c23035e61c74ab2
kych4n <hades1@kakao.com>
2025-08-19 22:07:31 +0900

    Chore [#1] 프론트엔드 배포 서버 설정

--- src/main/java/cotato/timetile/global/config/SecurityConfig.java
+++ src/main/java/cotato/timetile/global/config/SecurityConfig.java
@@ -108,7 +108,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173"));
+        config.setAllowedOrigins(
+                List.of("http://localhost:3000", "http://localhost:5173", "https://11th-time-tile-fe.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
